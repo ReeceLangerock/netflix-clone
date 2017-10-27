dda9be86b957f50f8c18be17ed974fce42584bf3
ReeceLangerock <ReeceLangerock@gmail.com>
2017-10-27 18:34:17 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+
+import Index from './components/index/Index.js'
+import WhosWatching from './components/whos_watching/WhosWatching.js'
+
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './redux/store/store'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Container>
+            <Page>
+
+              <main>
+                <Switch>
+                  <Route exact path='/' component={Index} />
+
+                  <Route path="/who" component={WhosWatching} />
+                </Switch>
+              </main>
+            </Page>
+          </Container>
+        </ConnectedRouter>
+      </Provider>
+    )
+  }
+}
+
+export default App
+
+const Page = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+max-width: 100vw;
+margin: 0 auto;
+min-height: 100vh;
+position: relative;
+`
+
+const Container = styled.div`
+  overflow: hidden;
+  max-width: 100vw;
+
+
+`
---
+++ src/styleguide.js
@@ -0,0 +1,58 @@
+export default {
+  // FONTS
+  fonts: {
+    color: {
+      alert: 'red',
+      dark: 'black',
+      light: 'white'
+    },
+
+    size: {
+      small: '10px',
+      medium: '16px',
+      large: '1.5rem'
+    }
+  },
+
+  // BACKGROUNDS
+  background: {
+    color: {
+      light: '#f4f1ea',
+      dark: 'grey'
+    }
+  },
+
+  button: {
+    padding: '7px 17px',
+    color: '#fff',
+    backgroundColor: '#e50914',
+    fontSize: '16px',
+    borderRadius: '3px',
+
+    hover: {
+      backgroundColor: ' #f40612'
+    }
+  },
+
+  buttonBig: {
+    padding: '12px 2em',
+    color: '#fff',
+    backgroundColor: '#e50914',
+    fontSize: '14px',
+    borderRadius: '3px',
+    letterSpacing: "1.9px",
+
+    hover: {
+      backgroundColor: ' #f40612'
+    }
+  },
+
+  // BORDERS
+  border: {
+    shadow: {
+      shadowBorder: '0 3px 6px rgba(118, 33, 33, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);'
+    }
+  },
+
+  lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+}
